3c26eea07df7b68fa299743cde12993e33b81011
Melanie Sumner <melaniersumner@gmail.com>
2021-06-17 16:05:27 -0500

    Added function to match against Map of todo datums and added tests

--- __tests__/match-utils-test.ts
+++ __tests__/match-utils-test.ts
@@ -1,6 +1,16 @@
-import { isFuzzyMatch } from "../src/match-utils";
+import { isFuzzyMatch, hasFuzzyMatch } from "../src/match-utils";
 import { getDatePart } from '../src/date-utils';
-import { TodoData } from "../src";
+import { TodoData, FilePath } from "../src";
+
+const todoDatumTemplate: TodoData = {
+  engine: 'ember-template-lint',
+  filePath: 'app/components/my-input.hbs',
+  ruleId: 'require-input-label',
+  line: 1,
+  column: 1,
+  createdDate: getDatePart().getTime(),
+  source: '<input/>'
+};
 
 describe('isFuzzyMatch', () => {
   let today: number;
@@ -161,5 +171,76 @@ describe('isFuzzyMatch', () => {
       source: '<input/>'
     }
     expect(isFuzzyMatch(testTodo, refTodo)).toEqual(false);
+  });
+});
+
+describe('isFuzzyMatch from template TodoData objects', () => {
+
+  it('successfully identifies a fuzzy match when the line number has changed', () => {
+    const testTodo: TodoData = todoDatumTemplate;
+    const refTodo: TodoData = Object.assign({}, todoDatumTemplate, { line: 2 } );
+    expect(isFuzzyMatch(testTodo, refTodo)).toEqual(true);
+  });
+});
+
+describe('hasFuzzyMatch', () => {
+  let today: number;
+  beforeEach(() => {
+    today = getDatePart().getTime();
+  });
+
+    // Relevant properties of default template:
+    //   filePath: 'app/components/my-input.hbs',
+    //   ruleId: 'require-input-label',
+    //   line: 1, column: 1,
+    //   source: '<input/>'
+
+  it('successfully identifies a fuzzy match within a refMap when the line number has changed', () => {
+    const testTodo: TodoData = todoDatumTemplate;
+    const refTodoMap = new Map<FilePath, TodoData>();
+    refTodoMap.set("hash01", Object.assign({}, todoDatumTemplate, { filepath: 'app/components/my-other-input.hbs' } ));
+    refTodoMap.set("hash02", Object.assign({}, todoDatumTemplate, { ruleId: 'some-other-rule' } ));
+    refTodoMap.set("hash03", Object.assign({}, todoDatumTemplate, { source: '<otherInput>' } ));
+    refTodoMap.set("needle", Object.assign({}, todoDatumTemplate, { line: 2 } ));
+    expect(hasFuzzyMatch(testTodo, refTodoMap)).toEqual(true);
+  });
+
+  it('successfully identifies a fuzzy match within a refMap when the column has changed', () => {
+    const testTodo: TodoData = todoDatumTemplate;
+    const refTodoMap = new Map<FilePath, TodoData>();
+    refTodoMap.set("hash01", Object.assign({}, todoDatumTemplate, { filepath: 'app/components/my-other-input.hbs' } ));
+    refTodoMap.set("hash02", Object.assign({}, todoDatumTemplate, { ruleId: 'some-other-rule' } ));
+    refTodoMap.set("hash03", Object.assign({}, todoDatumTemplate, { source: '<otherInput>' } ));
+    refTodoMap.set("needle", Object.assign({}, todoDatumTemplate, { column: 2 } ));
+    expect(hasFuzzyMatch(testTodo, refTodoMap)).toEqual(true);
+  });
+
+  it('successfully identifies a fuzzy match within a refMap when the line and column have changed', () => {
+    const testTodo: TodoData = todoDatumTemplate;
+    const refTodoMap = new Map<FilePath, TodoData>();
+    refTodoMap.set("hash01", Object.assign({}, todoDatumTemplate, { filepath: 'app/components/my-other-input.hbs' } ));
+    refTodoMap.set("hash02", Object.assign({}, todoDatumTemplate, { ruleId: 'some-other-rule' } ));
+    refTodoMap.set("hash03", Object.assign({}, todoDatumTemplate, { source: '<otherInput>' } ));
+    refTodoMap.set("needle", Object.assign({}, todoDatumTemplate, { line: 2, column: 2 } ));
+    expect(hasFuzzyMatch(testTodo, refTodoMap)).toEqual(true);
+  });
+
+  it('does not find a fuzzy match within a refMap containing exclusively todo data that has different filepath/ruleId/source properties', () => {
+    const testTodo: TodoData = todoDatumTemplate;
+    const refTodoMap = new Map<FilePath, TodoData>();
+    refTodoMap.set("hash01", Object.assign({}, todoDatumTemplate, { filepath: 'app/components/my-other-input.hbs' } ));
+    refTodoMap.set("hash02", Object.assign({}, todoDatumTemplate, { ruleId: 'some-other-rule' } ));
+    refTodoMap.set("hash03", Object.assign({}, todoDatumTemplate, { source: '<otherInput>' } ));
+    expect(hasFuzzyMatch(testTodo, refTodoMap)).toEqual(false);
+  });
+
+  it('does not find a fuzzy match within a refMap containing an exact match', () => {
+    const testTodo: TodoData = todoDatumTemplate;
+    const refTodoMap = new Map<FilePath, TodoData>();
+    refTodoMap.set("hash01", Object.assign({}, todoDatumTemplate, { filepath: 'app/components/my-other-input.hbs' } ));
+    refTodoMap.set("hash02", Object.assign({}, todoDatumTemplate, { ruleId: 'some-other-rule' } ));
+    refTodoMap.set("hash03", Object.assign({}, todoDatumTemplate, { source: '<otherInput>' } ));
+    refTodoMap.set("needle", Object.assign({}, todoDatumTemplate));
+    expect(hasFuzzyMatch(testTodo, refTodoMap)).toEqual(false);
   });
 });
--- src/match-utils.ts
+++ src/match-utils.ts
@@ -1,4 +1,4 @@
-import { TodoData } from './types';
+import { TodoData, FilePath } from './types';
 
 /**
  * Checks to see if a todo might be a fuzzy match.
@@ -8,14 +8,23 @@ import { TodoData } from './types';
  */
 
 export function isFuzzyMatch(testTodo: TodoData, refTodo: TodoData): boolean {
-  // if the filePath, ruleId and source match
   if (
     testTodo.filePath === refTodo.filePath &&
     testTodo.ruleId === refTodo.ruleId &&
     testTodo.source === refTodo.source &&
-  // if the line or column (or both) have changed
     (testTodo.line !== refTodo.line || testTodo.column !== refTodo.column)
   ) {
       return true;
   } return false;
+}
+
+/**
+ * Checks to see if a todo has a fuzzy match within a reference Map
+ * @param testTodo - The todo data needle to look for
+ * @param refTodoMap - The reference Map whose values are the todo haystack to look in
+ * @returns boolean - the testTodo fuzzy matches against at least one todo in the reference Map
+ * Note: a fuzzy match is false for an exact match
+ */
+export function hasFuzzyMatch(testTodo: TodoData, refTodoMap: Map<FilePath, TodoData>): boolean {
+  return [...refTodoMap.values()].some(refTodoDatum => isFuzzyMatch(testTodo, refTodoDatum));
 }
